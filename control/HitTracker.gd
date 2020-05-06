e41d4818249a2e072f4ed1511a0735338cb5c421
Luis Lopez <luisl@stellarscience.com>
2020-05-06 13:04:41 -0600

    Add word tracking
    A signal triggers when a word is completed.

--- control/HitTracker.gd
+++ control/HitTracker.gd
@@ -4,7 +4,19 @@ class_name HitTracker
 var _target = null setget ,get_target
 var _label = null
 var _text = ""
+
+# The global cursor for the entire text.
 var _hit_cursor:int = 0
+
+# Keeps of the currently tracked word
+var _current_word_index:int = 0
+
+# Keeps track of the local cursor of the current word.
+var _current_word_cursor:int = 0
+
+# Emits when the cursor passes end of word.
+# If a text target has multiple words, then it triggers at the end of each word.
+signal word_completed
 
 func _init(target, label):
   _target = target
@@ -30,5 +42,20 @@ func hit(letter:String) -> bool:
   if not is_done() and letter == _text[_hit_cursor]:
     _label.increment_cursor()
     _hit_cursor += 1
+    _current_word_cursor += 1
+    _track_word_finish()
     return true
   return false
+
+func _track_word_finish():
+  if _current_word_index < _word_count():
+    if _current_word_cursor == _current_word().length():
+      _current_word_index += 1 # Next word
+      _current_word_cursor = 0 # Reset cursor for next word
+      emit_signal("word_completed")
+
+func _word_count() -> int:
+  return _label.display_text.text_list.size()
+
+func _current_word() -> String:
+  return _label.display_text.text_list[_current_word_index]
