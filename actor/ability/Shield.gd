e8e4b8dd18bbb77c2ad78f7fbd2d139d8d31f1a6
Luis Lopez <luisl@stellarscience.com>
2020-05-19 15:50:30 -0600

    Timeout for shield, generalize groups of interest for time stream

--- actor/ability/Shield.gd
+++ actor/ability/Shield.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
+export var duration:float = 5.0
+
+func _ready():
+  var timer = $Timer
+  timer.wait_time = duration
+  timer.one_shot = true
+  timer.start()
+
 func _on_Shield_area_entered(other):
   if other.is_in_group("BaseActor"):
     other.get_node("Health").instakill()
+
+func _on_Timer_timeout():
+  queue_free()
--- actor/ability/TimeStream.gd
+++ actor/ability/TimeStream.gd
@@ -10,16 +10,19 @@ func _ready():
   timer.start()
 
 func _on_TimeStream_area_entered(other):
-  if other.is_in_group("BaseActor"):
+  if _is_in_groups_of_interest(other):
     other.motion.motion_scale = slow_scale
 
 func _on_TimeStream_area_exited(other):
-  if other.is_in_group("BaseActor"):
+  if _is_in_groups_of_interest(other):
     other.motion.motion_scale = 1.0
 
 # TODO CHECK: Does queue free call area exited?
 func _on_Timer_timeout():
   for area in get_overlapping_areas():
-    if area.is_in_group("BaseActor"):
+    if _is_in_groups_of_interest(area):
       area.motion.motion_scale = 1.0
   queue_free()
+
+func _is_in_groups_of_interest(target):
+  return target.is_in_group("BaseActor")
