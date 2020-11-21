d4f25a5c8e6a6ce17e6c8bba7713d2e32017c9e9
Luis Lopez <luisl@stellarscience.com>
2020-11-20 23:36:36 -0600

    Add naval canon

--- actor/ship/Player.gd
+++ actor/ship/Player.gd
@@ -2,35 +2,27 @@ class_name Player
 
 extends Area2D
 
-export(float) var default_angle = 0 setget _set_default_angle
 export(float) var rotation_smoothing = 4.0
 export var bullet_resource:PackedScene
 
 var aimed_target = weakref(null) setget _set_aimed_target, _get_aimed_target
 
-onready var sprite = $Sprite as Sprite
-onready var _gun_animation = $Sprite/Gun/AnimationPlayer
-onready var _gun_location = $Sprite/Gun
+onready var _gun_animation = $Gun/AnimationPlayer
+onready var _gun_location = $Gun
 
 func _ready():
-  $Sprite/Gun/Flash.visible = false
+  $Gun/Flash.visible = false
 
 func _process(delta):
-  rotate_sprite(delta)
+  _rotate_aim(delta)
 
 func aim_rotation_angle() -> float:
   var target = aimed_target.get_ref()
-  if target:
-    return get_angle_to(target.position)
-  else:
-    return default_angle
+  return global_position.direction_to(target.global_position).angle() if target else rotation
 
-func rotate_sprite(delta:float):
-  var angle = lerp_angle(sprite.rotation, aim_rotation_angle(), delta * rotation_smoothing)
-  sprite.set_rotation(angle)
-
-func _set_default_angle(value):
-  default_angle = deg2rad(value)
+func _rotate_aim(delta:float):
+  var angle = lerp_angle(rotation, aim_rotation_angle(), delta * rotation_smoothing)
+  set_rotation(angle)
 
 func _set_aimed_target(value):
   aimed_target = weakref(value)
@@ -46,7 +38,7 @@ func _play_gun():
 func fire_at_target(target, is_critical:bool):
   var bullet = Projectiles.spawn(
     bullet_resource,
-    _gun_location.position,
+    _gun_location.global_position,
     target)
   bullet.get_node("Damage").is_critical = is_critical
 
