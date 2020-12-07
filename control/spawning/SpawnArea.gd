8ab5cdf547c4e1265e6add82d83c113d2bf41b55
Luis Lopez <luisl@stellarscience.com>
2020-12-06 19:32:22 -0600

    Remove debug draw addon
    Cube mesh is now used to do debug visuals for spawn area.
    
    Debug draw addon tool could not be used as a tool script since it added
    meshes multiple times.

--- control/spawning/SpawnArea.gd
+++ control/spawning/SpawnArea.gd
@@ -5,11 +5,8 @@ class_name SpawnArea, "res://icons/area_position_icon.png"
 export var length:float = 10.0 setget _set_length
 export var width:float = 10.0 setget _set_width
 export var height:float = 10.0 setget _set_height
-export var debug_color:Color = Color(0.8, 0.4, 0.0, 1.0) setget _set_debug_color
 
 var _box := AABB(Vector3.ZERO, Vector3.ONE)
-
-onready var _debug_draw := $DebugDraw
 
 func spawn_position() -> Vector3:
   var x = rand_range(_box.position.x, _box.end.x)
@@ -21,21 +18,29 @@ func _set_length(value):
   length = value
   _box.size.x = value
   _box.position.x = -value / 2.0
+  if Engine.editor_hint:
+    var v = _visual()
+    if v:
+      v.size.x = value
 
 func _set_width(value):
   width = value
   _box.size.z = value
   _box.position.z = -value / 2.0
+  if Engine.editor_hint:
+    var v = _visual()
+    if v:
+      v.size.z = value
 
 func _set_height(value):
   height = value
   _box.size.y = value
   _box.position.y = -value / 2.0
-
-func _set_debug_color(value:Color):
-  debug_color = value
-
-func _process(_delta):
   if Engine.editor_hint:
-    var size = Vector3(length, height, width)
-    _debug_draw.draw_box(global_transform.origin - (size / 2.0), size, debug_color)
+    var v = _visual()
+    if v:
+      v.size.y = value
+
+func _visual() -> CubeMesh:
+  var v = get_node_or_null("Visual")
+  return v.mesh as CubeMesh if v else null
