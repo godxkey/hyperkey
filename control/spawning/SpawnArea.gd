0232070f8bd3f959a4b955d69c84ecbc75411c29
Luis Lopez <luisl@stellarscience.com>
2020-12-06 20:20:13 -0600

    Remove mesh visual for spawn area

--- control/spawning/SpawnArea.gd
+++ control/spawning/SpawnArea.gd
@@ -1,4 +1,3 @@
-tool
 extends Spatial
 class_name SpawnArea, "res://icons/area_position_icon.png"
 
@@ -18,29 +17,13 @@ func _set_length(value):
   length = value
   _box.size.x = value
   _box.position.x = -value / 2.0
-  if Engine.editor_hint:
-    var v = _visual()
-    if v:
-      v.size.x = value
 
 func _set_width(value):
   width = value
   _box.size.z = value
   _box.position.z = -value / 2.0
-  if Engine.editor_hint:
-    var v = _visual()
-    if v:
-      v.size.z = value
 
 func _set_height(value):
   height = value
   _box.size.y = value
   _box.position.y = -value / 2.0
-  if Engine.editor_hint:
-    var v = _visual()
-    if v:
-      v.size.y = value
-
-func _visual() -> CubeMesh:
-  var v = get_node_or_null("Visual")
-  return v.mesh as CubeMesh if v else null
